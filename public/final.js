d15b44855fd747465eeb82e1f2e8fc0460126991
Jeffrey Lowe <Jeffrey.lowe@primarysolutions.net>
2017-06-19 10:36:57 -0400

    Testing final stage

--- public/final.js
+++ public/final.js
@@ -1,7 +1,7 @@
 function doIt() {
 	$("#button").attr("disabled", true);
-	$("body").addClass("evilbody").append("<div>Thank you so much for all of your help! I wish I could pay you back somehow, but whenever you hear my idea, think of me! I hope you think I'm cool forever LOL</div>");
+	$("body").addClass("evilbody").append("<br /><div>Thank you so much for all of your help! I wish I could pay you back somehow, but whenever you hear my idea, think of me! I hope you think I'm cool forever LOL</div>");
 	setTimeout(function() {
 		$("body").append("<div class='evilreturns'><a href='fake'>>:)</a></div>");
-	}, 1000 * 20);
+	}, 1000 * 25);
 }
