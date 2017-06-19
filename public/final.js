d1b3d5d705eff4dc2d31970611214b6117ee862e
Jeffrey Lowe <Jeffrey.lowe@primarysolutions.net>
2017-06-19 10:39:01 -0400

    God damned typo

--- public/final.js
+++ public/final.js
@@ -2,6 +2,6 @@ function doIt() {
 	$("#button").attr("disabled", true);
 	$("body").addClass("evilbody").append("<br /><div>Thank you so much for all of your help! I wish I could pay you back somehow, but whenever you hear my idea, think of me! I hope you think I'm cool forever LOL</div>");
 	setTimeout(function() {
-		$("body").append("<div class='evilreturns'><a href='fake'>>:)</a></div>");
-	}, 1000 * 25);
+		$("body").append("<br /><div class='evilreturns'><a href='fake'>>:)</a></div>");
+	}, 1000 * 20);
 }
