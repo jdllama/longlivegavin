f04b8dd17c345682cb594fba0d5b750d31deac10
Jeffrey Lowe <Jeffrey.lowe@primarysolutions.net>
2017-06-19 12:07:10 -0400

    Minor changes

--- public/final.js
+++ public/final.js
@@ -3,5 +3,6 @@ function doIt() {
 	$("body").addClass("evilbody").append("<br /><div>Thank you so much for all of your help! I wish I could pay you back somehow, but whenever you hear my idea, think of me! I hope you think I'm cool forever LOL</div>");
 	setTimeout(function() {
 		$("body").append("<br /><div class='evilreturns'><a href='fake'>>:)</a></div>");
+		document.title = "See you around, amigo";
 	}, 1000 * 20);
 }
