2cd46783726c2b0601b7d3ce493bbcc1fe1290af
Jeffrey Lowe <Jeffrey.lowe@primarysolutions.net>
2017-06-19 10:29:31 -0400

    Testing final stage

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ app.get('/HSIM7/PMOSH', function(request, response) {
   response.render('pages/stage3');
 });
 
+app.get("/JDtest", function(request, response) {
+  response.render('pages/finalstage');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
