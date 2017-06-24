9e9ddd7d8cc56dac80e6ae2f0067c4a4b0d65434
J.D. Lowe <jdlowe.dev@gmail.com>
2017-06-23 22:17:12 -0400

    Documentation stuffs

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ app.get("/JDadmin", function(req, res) {
 app.post(finalURL, function(req, res) {
   var idea = req.body.idea;
   if(!idea) idea = "";
-  allGuesses.push({IP: req.connection.remoteAddress, idea: idea})
+  allGuesses.push({IP: req.headers['x-forwarded-for'], idea: idea})
   idea = idea.toUpperCase().trim();
   var goodbyeMessage = "YES! It worked! I can feel my essence changing into a song idea now. Now, every time you hear Smash Mouth - All Star, I will live on even for the briefest of moments! I don't know if we will meet again, but if so, I will miss you.<br /><br />Although it's weird, I feel like something's following me LOL";
   var correctScript = "$('#response').html('');$('body').append('<link rel=\"stylesheet\" type=\"text/css\" href=\"http://csshake.surge.sh/csshake.min.css\"><link rel=\"stylesheet\" type=\"text/css\" href=\"/style.css\" />'); $('#holder').addClass('shake-opacity');$('#button').prop('disabled', true);setTimeout(function() {$('#holder').remove(); $('body').append(\"<audio id='player'><source src='1.mp3'></audio>\");setTimeout(function() {$('#player')[0].play();}, 2000); setTimeout(function() {$('body').addClass('body');$('#response').html(\"" + goodbyeMessage + "\");setTimeout(function() {$('body').append(\"<br /><div class='TJWGr6D8kZs'><a href='ThankYouDanger' title='See you around, amigo' alt='See you around, amigo'>>:)</a></div>\");document.title = 'See you around, amigo';}, 1000 * 20);}, 2000);}, 3000);";
