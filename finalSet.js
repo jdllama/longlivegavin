c9a298284eb415d7770c5c9eecc43f9582630b4d
J.D. Lowe <jdlowe.dev@gmail.com>
2017-07-01 23:11:51 -0400

    Tiny touchups

--- finalSet.js
+++ finalSet.js
@@ -6,8 +6,19 @@ module.exports = function(app) {
         response.redirect('/404');
     });
 
+    app.get("/DEFENESTRATE", function(request, response) {
+        if(request.cookies.GO_FOR_THE_GOOD_ENDING) {
+            return response.render("pages/newgameultra/stage3");
+        }
+        response.redirect('/404');
+    });
+
     app.get('/DIRIGIBLE/MARIO', function(request, response) {
         response.redirect('/MARIO');
+    });
+
+    app.get('/MARIO/DEFENESTRATE', function(request, response) {
+        response.redirect('/DEFENESTRATE');
     });
 
     app.get('/MARIO/PMOSH.rar', function(request, response) {
