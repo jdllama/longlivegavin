e07ceecf5269d644008761cbbe5fd8bf72e4a86a
J.D. Lowe <jdlowe.dev@gmail.com>
2017-07-02 19:36:27 -0400

    Stage 3 and 4 NGU puzzles

--- finalSet.js
+++ finalSet.js
@@ -13,12 +13,23 @@ module.exports = function(app) {
         response.redirect('/404');
     });
 
+    app.get("/GROUNDBREAKING", function(request, response) {
+        if(request.cookies.GO_FOR_THE_GOOD_ENDING) {
+            return response.render("pages/newgameultra/stage4");
+        }
+        response.redirect('/404');
+    });
+
     app.get('/DIRIGIBLE/MARIO', function(request, response) {
         response.redirect('/MARIO');
     });
 
     app.get('/MARIO/DEFENESTRATE', function(request, response) {
         response.redirect('/DEFENESTRATE');
+    });
+
+    app.get('/DEFENESTRATE/GROUNDBREAKING', function(request, response) {
+        response.redirect('/GROUNDBREAKING');
     });
 
     app.get('/MARIO/PMOSH.rar', function(request, response) {
